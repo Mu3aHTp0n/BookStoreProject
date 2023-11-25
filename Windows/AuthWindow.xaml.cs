2a8c8687645159ef774cca4812849c962999ec0f
Mu3aHTp0n <kovalevartur05@gmail.com>
2023-11-25 21:46:35 +0300

    debug file DB & add AuthWindow

--- Windows/AuthWindow.xaml.cs
+++ Windows/AuthWindow.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BookStoreProject.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AuthWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthWindow : Window
     {
-        public MainWindow()
+        public AuthWindow()
         {
             InitializeComponent();
         }
